3ff45a8cac305497cde0eaf4a982add958f78249
Gabriel Tyler <gabriel.tyler16@gmail.com>
2022-12-02 19:54:12 -0600

    Finish day1 part2

--- Day1-CalorieCounting/main2.cpp
+++ Day1-CalorieCounting/main2.cpp
@@ -2,7 +2,7 @@
  * Gabriel Tyler
  * Advent of Code 2022
  * Day 1 part 2
- * Answer: 
+ * Answer: 193697
  */
 
 #include <iostream>
@@ -37,7 +37,9 @@ void solve()
 {
     ll curr = 0;
     ll total = 0;
-    ll max = 0;
+    ll max1 = 0; // first most
+    ll max2 = 0; // second most
+    ll max3 = 0; // third most
 
     string s;
     int num_empty_lines = 0;
@@ -46,8 +48,23 @@ void solve()
         // blank line if s is empty
         if (s.empty()) 
         {
-            if (total > max) 
-                max = total;
+            // update the maxes
+            if (total >= max1)
+            {
+                max3 = max2;
+                max2 = max1;
+                max1 = total;
+            }
+            else if (total >= max2)
+            {
+                max3 = max2;
+                max2 = total;
+            }
+            else if (total >= max3)
+            {
+                max3 = total;
+            }
+
             total = 0;
             ++num_empty_lines;
             continue;
@@ -74,10 +91,23 @@ void solve()
     }
     
     // last check because the input may not end with a blank line
-    if (total > max)
-        max = total;
+    if (total >= max1)
+    {
+        max3 = max2;
+        max2 = max1;
+        max1 = total;
+    }
+    else if (total >= max2)
+    {
+        max3 = max2;
+        max2 = total;
+    }
+    else if (total >= max3)
+    {
+        max3 = total;
+    }
 
-    cout << max << '\n';    
+    cout << add(max1, add(max2, max3)) << '\n';    
 }
 
 int main()
