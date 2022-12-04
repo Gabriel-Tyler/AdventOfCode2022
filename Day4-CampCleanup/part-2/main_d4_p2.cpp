8968507ac1cb097bb81fc1c043703f3b80bb0eda
Gabriel Tyler <gabriel.tyler16@gmail.com>
2022-12-04 00:43:19 -0600

    Finish Day 4 part 2

---
+++ Day4-CampCleanup/part-2/main_d4_p2.cpp
@@ -0,0 +1,73 @@
+/**
+ * Gabriel Tyler
+ * Advent of Code 2022
+ * Day 4 part 2
+ * Answer: 905
+ */
+
+#include <iostream>
+#include <string>
+
+#define forn(i, n) for (int i = 0; i < n; ++i)
+
+using namespace std;
+
+using ll = long long;
+
+void solve()
+{
+    int num_overlaps = 0;
+    string line;
+    while (getline(cin, line))
+    {
+        if (line.empty())
+            break;
+
+        // parse the line into a pair of ranges ([1st/2nd elf][left/right in range])
+        ll range_pair[2][2];
+
+        forn (i, 2)
+            forn (j, 2)
+                range_pair[i][j] = 0;
+
+        size_t curr = 0;
+        size_t pos = 0;
+        forn (i, 2)
+        {
+            forn (j, 2)
+            {
+                try
+                {
+                    range_pair[i][j] = stoll(line.substr(curr), &pos);
+                    curr += pos+1;
+                }
+                catch(const invalid_argument& e)
+                {
+                    cout << "invalid_argument::what(): " << e.what() << '\n';
+                }
+                catch(const out_of_range& e)
+                {
+                    cout << "out_of_range::what(): " << e.what() << '\n';
+                }
+            }
+        }
+        
+        // check the boundaries of each range pair to see if they overlap at all
+        if ((range_pair[0][0] <= range_pair[1][0] && range_pair[1][0] <= range_pair[0][1]) ||
+            (range_pair[1][0] <= range_pair[0][0] && range_pair[0][0] <= range_pair[1][1]))
+        {
+            ++num_overlaps;
+        }
+
+    }
+    cout << num_overlaps << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
+
