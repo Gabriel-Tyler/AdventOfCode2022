90eee2111e327ad69c6d39d2ad51469bd7536ff8
Gabriel Tyler <gabriel.tyler16@gmail.com>
2022-12-03 23:06:18 -0600

    Start day 4 part 1

---
+++ Day4-CampCleanup/part-1/main_d4_p1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define fore(i, l, r) for (int i = l; i < r; ++i)
+
+#define scasti(x) static_cast<int>(x)
+
+using namespace std;
+
+using ll = long long;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
+
