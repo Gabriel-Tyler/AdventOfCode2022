528b4c4709aee20e19f5639139bea44b1dddd899
Gabriel Tyler <gabriel.tyler16@gmail.com>
2022-12-04 22:51:50 -0600

    init day 5

---
+++ Day5-/part-2/main_d5_p2.cpp
@@ -0,0 +1,27 @@
+/**
+ * Gabriel Tyler
+ * Advent of Code 2022
+ * Day 5 part 2
+ * Answer: 
+ */
+
+#include <bits/stdc++.h>
+
+#define forn(i, n) for (int i = 0; i < n; ++i)
+
+using namespace std;
+
+using ll = long long;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
+
