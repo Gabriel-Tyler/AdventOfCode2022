9f76870ae4ae0a04e6047d371ef08362116a0172
Gabriel Tyler <gabriel.tyler16@gmail.com>
2022-12-05 00:17:59 -0600

    Finish Day5 part 2

--- Day5-SupplyStacks/part-2/main_d5_p2.cpp
+++ Day5-SupplyStacks/part-2/main_d5_p2.cpp
@@ -2,7 +2,7 @@
  * Gabriel Tyler
  * Advent of Code 2022
  * Day 5 part 2
- * Answer: 
+ * Answer: "TPWCGNCCG"
  */
 
 #include <bits/stdc++.h>
@@ -13,14 +13,68 @@ using namespace std;
 
 using ll = long long;
 
+struct Operation
+{
+    int num, src, dst;
+};
+
 void solve()
 {
+    // get the stacks
+    vector<string> lines;
+    string line;
+    while (getline(cin, line) && line[1] != '1')
+    {
+        lines.push_back(line);
+    }
+
+    int num_stacks = (line.size()-1)/4+1;
+    vector<stack<char>> stacks(num_stacks);
+    for (int i = lines.size()-1; i >= 0; --i)
+    {
+        forn (j, num_stacks)
+        {
+            char c = lines[i][4*j+1];
+            if (c != ' ')
+                stacks[j].push(lines[i][4*j+1]);
+        }
+    }
+
+    // get the ops
+    getline(cin, line);
+    while (getline(cin, line) && !line.empty())
+    {
+        Operation op;
+        string s;
+        istringstream iss(line);
+        iss >> s >> op.num >> s >> op.src >> s >> op.dst;
+
+        stack<char> top_of_stack;
+        for (int i = 0; i < op.num; ++i)
+        {
+            top_of_stack.push(stacks[op.src-1].top());
+            stacks[op.src-1].pop();
+        }
+        while (!top_of_stack.empty())
+        {
+            stacks[op.dst-1].push(top_of_stack.top());
+            top_of_stack.pop();
+        }
+    }
+
+    // get the top of each stack
+    // m 1 to 2
+    forn (i, num_stacks)
+    {
+        cout << stacks[i].top();
+    }
+    cout << '\n';
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(nullptr);
 
     solve();
 }
