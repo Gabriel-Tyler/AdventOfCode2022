743824cd16ab5cb4283ee0688c12a35b4ed53f89
Gabriel Tyler <gabriel.tyler16@gmail.com>
2022-12-06 22:54:24 -0600

    Start day 7

---
+++ Day7/part-2/main_d7_p2.cpp
@@ -0,0 +1,24 @@
+/**
+ * Answer: 
+ */
+
+#include <bits/stdc++.h>
+
+#define forn(i, n) for (int i = 0; i < n; ++i)
+
+using namespace std;
+
+using ll = long long;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
+
