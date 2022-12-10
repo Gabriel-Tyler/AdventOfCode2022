789f59c023913bd6a01ec941b17bb8b80e684cf3
Gabriel Tyler <gabriel.tyler16@gmail.com>
2022-12-10 00:21:45 -0600

    Start day 9

--- Day9-RopeBridge/part-1/main.cpp
+++ Day9-RopeBridge/part-1/main.cpp
@@ -7,25 +7,13 @@ using namespace std;
 
 class Solution
 {
-private:
-
-    int hx = 500, hy = 500, tx = 500, ty = 500;
-    vector<vector<bool>> visited(1000, vector<bool>(1000, false));
-
 public:
     void Solve()
     {
         string line;
         while (getline(cin, line) && !line.empty())
-       {
-   //         istringstream iss(line);
-     //       char dir;
-       //     int num;
-         //   iss >> dir >> num;
-
-
-//        }
-    }
+        {
+        }
 };
 
 int main()
